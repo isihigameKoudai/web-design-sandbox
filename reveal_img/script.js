1232c48840777742c917d6c334db46139ffc81a3
koudai ishigame <koudaiishigame@koudainoMacBook-Pro.local>
2019-08-03 01:52:18 +0900

    reveal-img

---
+++ reveal_img/script.js
@@ -0,0 +1,23 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      title: 'aaaaaa',
+      imgLists: [{
+        path: './img/sample1.jpg', alt: ''
+      },{
+        path: './img/sample2.jpeg', alt: ''
+      },{
+        path: './img/sample3.jpeg', alt: ''
+      },{
+        path: './img/sample4.jpeg', alt: ''
+      },{
+        path: './img/sample5.jpg', alt: ''
+      },{
+        path: './img/sample6.jpg', alt: ''
+      },{
+        path: './img/sample7.jpg', alt: ''
+      }]
+    }
+  }
+});
