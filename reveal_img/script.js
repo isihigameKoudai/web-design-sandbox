b4e6734286388282f3f5f4ba3abb0fee6b5322ea
koudai ishigame <koudaiishigame@koudainoMacBook-Pro.local>
2019-08-03 13:32:28 +0900

    IntersectionObserverの追加

--- reveal_img/script.js
+++ reveal_img/script.js
@@ -2,22 +2,56 @@ new Vue({
   el: '#app',
   data() {
     return {
-      title: 'aaaaaa',
       imgLists: [{
-        path: './img/sample1.jpg', alt: ''
+        path: './img/sample1.jpg', alt: '', isShow: false
       },{
-        path: './img/sample2.jpeg', alt: ''
+        path: './img/sample2.jpeg', alt: '', isShow: false
       },{
-        path: './img/sample3.jpeg', alt: ''
+        path: './img/sample3.jpeg', alt: '', isShow: false
       },{
-        path: './img/sample4.jpeg', alt: ''
+        path: './img/sample4.jpeg', alt: '', isShow: false
       },{
-        path: './img/sample5.jpg', alt: ''
+        path: './img/sample5.jpg', alt: '', isShow: false
       },{
-        path: './img/sample6.jpg', alt: ''
-      },{
-        path: './img/sample7.jpg', alt: ''
+        path: './img/sample6.jpg', alt: '', isShow: false
       }]
+    }
+  },
+  mounted(){
+    this.setIntersectionObserver('.js-reveal')
+  },
+  methods: {
+    /**
+     * IntersectionObserverの設定
+     * @param {String} className DOM要素を取得する時のクラス名
+     */
+    setIntersectionObserver(className) {
+      // Observerの設定
+      const options = {
+        threshold: 1.0
+      };
+
+      const clientHeight = document.documentElement.clientHeight;
+      // IntersectionPbserverが作動した時の動作を取得したDOMに適用する
+      const observer = new IntersectionObserver(entries => {
+        for (const entry of entries) {
+          const rect = entry.target.getBoundingClientRect();
+          if (clientHeight > rect.top) {
+            entry.target.classList.add('show');
+          }
+        }
+      }, options);
+
+      // DOMを取得したのち、observerを対象のDOMに適用
+      const targets = document.querySelectorAll(className);
+      targets.forEach(target => observer.observe(target));
+    },
+    showImage(e) {
+      this.imgLists[e].isShow = true;
+
+    },
+    className(isShow) {
+      return isShow ? 'show' : '';
     }
   }
 });
