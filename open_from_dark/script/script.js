2545880851b5e5ca08ccb3da561958090e8d76fa
koudaiishigame <kira5052@gmail.com>
2019-09-03 21:13:06 +0900

    animation連結

--- open_from_dark/script/script.js
+++ open_from_dark/script/script.js
@@ -3,20 +3,12 @@ new Vue({
   el: '#app',
   data() {
     return {
-      isHidden: false,
-      isWide: false,
-      isOpen: false
+      isHidden: false
     }
   },
   methods: {
     buttonPush() {
       this.isHidden = true;
-      setTimeout(() => {
-        this.isWide = true;
-      }, 1300);
-      setTimeout(() => {
-        this.isOpen = true;
-      }, 2000);
     }
   }
 })
