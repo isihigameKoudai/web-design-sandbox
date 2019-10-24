fc3941c3283a61485a22309e46cc19c259d46ddf
koudaiishigame <kira5052@gmail.com>
2019-10-24 23:22:16 +0900

    add smooth page transition

--- insperation_br_mercury_os/script/script.js
+++ insperation_br_mercury_os/script/script.js
@@ -1,1 +1,42 @@
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      isFocus: true,
+      menus: [{
+        label: 'App', path: 'app'
+      },{
+        label: 'Notification', path: 'notification'
+      },{
+        label: 'Todo', path: 'todo'
+      },{
+        label: 'Memo', path: 'memo'
+      },{
+        label: 'Reminder', path: 'reminder'
+      },{
+        label: 'Photo', path: 'photo'
+      }]
+    }
+  },
+  mounted() {
+    this.setEventListener();
+  },
+  methods: {
+    ableFocus() {
+      this.isFocus = true;
+    },
+    disableFocus() {
+      this.isFocus = false;
+    },
+    setEventListener() {
+      const $input = document.querySelector('.search-box__text');
+      $input.addEventListener('focus', () => {
+        this.ableFocus();
+      })
 
+      $input.addEventListener('blur', () => {
+        this.disableFocus();
+      })
+    }
+  }
+})
