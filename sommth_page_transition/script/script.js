041df7253dbb1fe3744219feff56199801ad6a55
koudaiishigame <kira5052@gmail.com>
2020-01-11 20:49:57 +0900

    edit page transition

--- sommth_page_transition/script/script.js
+++ sommth_page_transition/script/script.js
@@ -1,1 +1,10 @@
-
+new Vue({
+  el: '#app',
+  methods: {
+    onMoveTo(e, path) {
+      e.preventDefault();
+      document.querySelector('body').classList.add('is-move');
+      setTimeout(()=> location.assign(path), 900);
+    }
+  }
+});
