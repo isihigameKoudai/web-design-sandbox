79d61e829b48a49a7e29cfe8c9da798bf087138a
koudaiishigame <kira5052@gmail.com>
2020-07-07 03:06:48 +0900

    hover時補正

--- mouse_cursor_design/script/script.js
+++ mouse_cursor_design/script/script.js
@@ -8,7 +8,15 @@ new Vue({
       mouseX: 0, //マウスのX座標
       mouseY: 0, //マウスのY座標
       posX: 0, //フォロワーのX座標
-      posY: 0 //フォロワーのX座標
+      posY: 0, //フォロワーのX座標
+      addSize: 0,
+      isHover: false,
+      hoverOn: {}
+    }
+  },
+  computed: {
+    hovered() {
+      return this.getRect(this.hoverOn);
     }
   },
   methods: {
@@ -31,18 +39,49 @@ new Vue({
         this.fWidth = 40;
         TweenMax.to($follower, 0.3, { width: this.fWidth, height: this.fWidth });
       });
+
+      const $hovereds = document.querySelectorAll('.js-hovered');
+      console.log($hovereds);
+      [...$hovereds].map(item => {
+        item.addEventListener('mouseover', e => {
+          const {x,y,w,h} = this.getRect(e.target);
+          this.addSize = 80;
+
+          this.isHover = true;
+          this.hoverOn = e.target;
+
+          const css = { width: w + this.addSize, height: h + this.addSize, borderRadius: 20 };
+          TweenMax.to($follower, 0.5, css);
+        })
+
+        item.addEventListener('mouseleave', e => {
+          this.fWidth = 40;
+          this.isHover = false;
+          TweenMax.to($follower, 0.3, { width: this.fWidth, height: this.fWidth, borderRadius: '50%' });
+        })
+      })
+
     },
     setTwennMax() {
       TweenMax.to({}, .0001, {
         repeat: -1,
         onRepeat: () => {
+
           this.posX += (this.mouseX - this.posX) / this.delay;
           this.posY += (this.mouseY - this.posY) / this.delay;
 
+          const { x, y, w, h} = this.hovered;
+          const targetTotalX = x + w;
+          const targetTotalY = y + h;
+          // console.log(x, y, w, h, this.posX, this.posY);
+
+          const followerX = this.isHover ? this.posX - ((this.addSize + w) / 2) : this.posX - (this.fWidth / 2);
+          const followerY = this.isHover ? this.posY - ((this.addSize + h) / 2) : this.posY - (this.fWidth / 2);
+
           TweenMax.set(this.$refs.follower, {
             css: {
-              left: this.posX - (this.fWidth / 2),
-              top: this.posY - (this.fWidth / 2)
+              left: followerX,
+              top: followerY
             }
           });
 
@@ -54,6 +93,27 @@ new Vue({
           });
         }
       });
+    },
+    getRect(target) {
+      if (!target) return {};
+
+      const targetX = target.getBoundingClientRect().left;
+      const targetY = target.getBoundingClientRect().top + window.pageYOffset;
+      const targetW = target.clientWidth;
+      const targetH = target.clientHeight;
+
+      return {
+        x: targetX,
+        y: targetY,
+        w: targetW,
+        h: targetH,
+        halfW: targetW / 2,
+        half: targetH / 2,
+        center: {
+          x: targetX + targetW / 2,
+          y: targetY + targetH / 2,
+        }
+      }
     }
   },
   mounted() {
